f9b5b1b84c0e5196cee145e6feae360975e4d099
majidanasrin <147602095+majidanasrin@users.noreply.github.com>
2024-03-27 07:03:24 +0530

    Add files via upload

---
+++ c2q2/Stack2.h
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+template <class T>
+class LinkedListStack {
+private:
+    Node<T>* top;
+    int maxSize; 
+    int size;
+
+public: 
+    LinkedListStack(int);
+    ~LinkedListStack();
+
+    void push(T value);
+    T pop();
+    bool isEmpty();
+    bool isFull();
+    void display();
+};
