fbdb508efdd12b3cac20e23b20632492abba55a2
majidanasrin <147602095+majidanasrin@users.noreply.github.com>
2024-05-04 17:28:02 +0530

    Add files via upload

---
+++ c2q9/PostfixEvaluation.cpp
@@ -0,0 +1,30 @@
+
+#include"PostfixEvaluation.h"
+
+template<typename T>
+void Stack<T> :: push(T element) {
+    data.push(element);
+}
+
+template<typename T>
+T Stack<T> :: pop() {
+    if (isEmpty()) {
+        throw StackException("Stack is empty");
+    }
+    T topElement = data.top();
+    data.pop();
+    return topElement;
+}
+
+template<typename T>
+T Stack<T> :: peek() {
+    if (isEmpty()) {
+        throw StackException("Stack is empty");
+    }
+    return data.top();
+}
+
+template<typename T>
+bool Stack<T> :: isEmpty() {
+    return data.empty();
+}
